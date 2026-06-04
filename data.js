6acb13dc38a1075fb493f724cd885556a33f444d
mrtzym016-tech <mrtzym016@gmail.com>
2026-06-04 12:19:56 +0300

    Update data.js

--- data.js
+++ data.js
@@ -2,56 +2,34 @@ const lessons = [
 
 {
 title:"Learn Arabic Daily Routine",
-
 category:"Daily Arabic",
-
-video:
-"https://youtu.be/TFa3Hi9I-Tw?si=xTMtSca8UxOwS8fT",
-
-pdf:
-"https://upfiles.com/l0e4a8h",
-
-quiz:"#"
+video:"https://youtu.be/TFa3Hi9I-Tw?si=xTMtSca8UxOwS8fT",
+pdf:"https://upfiles.com/l0e4a8h",
+quiz:null
 },
 
 {
 title:"Learn Arabic Through Daily Street Conversations",
-
 category:"Arabic Conversations",
-
-video:
-"https://youtu.be/K_xXl0-fVg8?si=Ov9foP_K0C-V4rbv",
-
-pdf:
-"https://linkjust.com/JbT1YYd",
-
-quiz:"#"
+video:"https://youtu.be/K_xXl0-fVg8?si=Ov9foP_K0C-V4rbv",
+pdf:"https://linkjust.com/JbT1YYd",
+quiz:null
 },
 
 {
 title:"Learn Arabic Through Real Arabic Conversation",
-
 category:"Arabic Conversations",
-
-video:
-"https://youtu.be/AZaFHJLLxR8?si=9RIFMm6ZpNcWmu1v",
-
-pdf:
-"https://linkjust.com/J6rR",
-
-quiz:"#"
+video:"https://youtu.be/AZaFHJLLxR8?si=9RIFMm6ZpNcWmu1v",
+pdf:"https://linkjust.com/J6rR",
+quiz:null
 },
 
 {
 title:"Learn Arabic Through Self Introduction",
-
 category:"Beginner Arabic",
-
-video:
-"https://youtu.be/X9i92mnxJ2k?si=OmcNGgtcuwtS66Wm",
-
-pdf:
-"https://linkjust.com/gXPO4",
-
-quiz:"#"
+video:"https://youtu.be/X9i92mnxJ2k?si=OmcNGgtcuwtS66Wm",
+pdf:"https://linkjust.com/gXPO4",
+quiz:null
 }
+
+];
