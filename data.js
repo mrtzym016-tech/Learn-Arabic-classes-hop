cb5843655f4f9cb7821b1fb5b84d2937a44b5b64
mrtzym016-tech <mrtzym016@gmail.com>
2026-06-04 11:53:59 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,32 +1,59 @@
 const lessons = [
 
 {
-title:"Arabic Greetings",
+title:"learn Arabic/daily Routine",
 category:"Beginner Arabic",
 
 video:
-"https://www.youtube.com/watch?v=123",
+"https://youtu.be/TFa3Hi9I-Tw?si=xTMtSca8UxOwS8fT",
 
 pdf:
-"https://example.com/file1.pdf",
+"https://upfiles.com/l0e4a8h",
 
 quiz:
-"https://example.com/quiz1"
+null
 },
 
 {
-title:"Introduce Yourself",
-
+title:"learn Arabic through daily street conversations",
 category:"Beginner Arabic",
 
 video:
-"https://www.youtube.com/watch?v=456",
+"https://youtu.be/K_xXl0-fVg8?si=Ov9foP_K0C-V4rbv",
 
 pdf:
-"https://example.com/file2.pdf",
+"https://linkjust.com/JbT1YYd",
 
 quiz:
-"https://example.com/quiz2"
+null
+},
+
+{
+title:"learn Arabic through Real Arabic conversation",
+category:"Beginner Arabic",
+
+video:
+"https://youtu.be/AZaFHJLLxR8?si=9RIFMm6ZpNcWmu1v",
+
+pdf:
+"https://linkjust.com/J6rR",
+
+quiz:
+null
+},
+
+{
+title:"learn Arabic through yourself introduction",
+category:"Beginner Arabic",
+
+video:
+"https://youtu.be/X9i92mnxJ2k?si=OmcNGgtcuwtS66Wm",
+
+pdf:
+"https://linkjust.com/gXPO4",
+
+quiz:
+null
 }
 
 ];
