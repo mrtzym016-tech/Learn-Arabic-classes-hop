fb015ab76605ca4f7684f92cf9e841b97eb288ac
mrtzym016-tech <mrtzym016@gmail.com>
2026-06-04 09:42:23 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,32 @@
+const lessons = [
+
+{
+title:"Arabic Greetings",
+category:"Beginner Arabic",
+
+video:
+"https://www.youtube.com/watch?v=123",
+
+pdf:
+"https://example.com/file1.pdf",
+
+quiz:
+"https://example.com/quiz1"
+},
+
+{
+title:"Introduce Yourself",
+
+category:"Beginner Arabic",
+
+video:
+"https://www.youtube.com/watch?v=456",
+
+pdf:
+"https://example.com/file2.pdf",
+
+quiz:
+"https://example.com/quiz2"
+}
+
+];
