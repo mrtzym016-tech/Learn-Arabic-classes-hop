8fa4a796b7c967ffebb399dcabea9c939720665c
mrtzym016-tech <mrtzym016@gmail.com>
2026-06-06 08:22:50 +0300

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const lessons = [
 {
 title:"Learn Arabic Daily Routine",
 category:"Daily Arabic",
-video:"https://youtu.be/TFa3Hi9I-Tw?si=xTMtSca8UxOwS8fT",
+video:"https://www.youtube.com/embed/TFa3Hi9I-Tw",
 pdf:"https://upfiles.com/l0e4a8h",
 quiz:null
 },
@@ -11,7 +11,7 @@ quiz:null
 {
 title:"Learn Arabic Through Daily Street Conversations",
 category:"Arabic Conversations",
-video:"https://youtu.be/K_xXl0-fVg8?si=Ov9foP_K0C-V4rbv",
+video:"https://www.youtube.com/embed/K_xXl0-fVg8",
 pdf:"https://linkjust.com/JbT1YYd",
 quiz:null
 },
@@ -19,7 +19,7 @@ quiz:null
 {
 title:"Learn Arabic Through Real Arabic Conversation",
 category:"Arabic Conversations",
-video:"https://youtu.be/AZaFHJLLxR8?si=9RIFMm6ZpNcWmu1v",
+video:"https://www.youtube.com/embed/AZaFHJLLxR8",
 pdf:"https://linkjust.com/J6rR",
 quiz:null
 },
@@ -27,7 +27,7 @@ quiz:null
 {
 title:"Learn Arabic Through Self Introduction",
 category:"Beginner Arabic",
-video:"https://youtu.be/X9i92mnxJ2k?si=OmcNGgtcuwtS66Wm",
+video:"https://www.youtube.com/embed/X9i92mnxJ2k",
 pdf:"https://linkjust.com/gXPO4",
 quiz:null
 }
